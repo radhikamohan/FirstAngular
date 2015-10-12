4bf4375c4dc790c0068956c24d99c79a8d72c8a1
Mohan Kaka <k.radhikamohan@gmail.com>
2015-10-12 19:11:46 -0400

    Added github search feature

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ var app = angular.module("gitHubViewer",[]);
 
 var MainController = function($scope, $http)
 {
+    $scope.username = "angular";
+    $scope.message = "GitHub Viewer";
   var onUserCompletion = function(response)
   {
-    $scope.user = response.data;
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url).then(onRepos, onError);
+  };
+
+  var onRepos = function (response) {
+      $scope.repos = response.data;
   };
   
   var onError = function(reason)
   {
-    $scope.error = reason;
+    $scope.error = "Couldn't fetch the data";
   };
   
-  $http.get("https://api.github.com/users/radhikamohan")
+  $scope.search = function()
+  {
+      $http.get("https://api.github.com/users/"+ $scope.username)
     .then(onUserCompletion, onError);
+  };
 };
-
-app.controller("MainController",MainController);
-  
+app.controller("MainController",MainController);  
 }());
 
